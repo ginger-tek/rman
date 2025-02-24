d48620ffbd1f33b5634c0f4c0b8402f749d0a010
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-02-24 15:48:54 +0000

    add dir creation

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,5 @@
 import fs from 'fs/promises'
+import { existsSync, mkdirSync } from 'fs'
 import { DateTime } from 'luxon'
 import { basename, dirname } from 'path'
 
@@ -6,21 +7,23 @@ export default class Logger {
   constructor(file) {
     this.name = basename(file)
     this.dir = dirname(file)
+    if (!existsSync(this.dir))
+      mkdirSync(this.dir, { recursive: true })
   }
   get path() {
     const date = DateTime.now().toFormat('yyyyMMdd')
     return `${this.dir}/${date}_${this.name}`
   }
   _clean(str) {
-    return str.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '')
+    return str.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '').trim()
   }
-  info(msg) {
-    fs.appendFile(this.path, `${DateTime.now().toUTC()} [INFO]  ${this._clean(msg).trim()}\n`)
+  async info(msg) {
+    fs.appendFile(this.path, `${DateTime.now().toUTC()} [INFO]  ${this._clean(msg)}\n`)
   }
-  warn(msg) {
-    fs.appendFile(this.path, `${DateTime.now().toUTC()} [WARN]  ${this._clean(msg).trim()}\n`)
+  async warn(msg) {
+    fs.appendFile(this.path, `${DateTime.now().toUTC()} [WARN]  ${this._clean(msg)}\n`)
   }
-  error(msg) {
-    fs.appendFile(this.path, `${DateTime.now().toUTC()} [ERROR] ${this._clean(msg).trim()}\n`)
+  async error(msg) {
+    fs.appendFile(this.path, `${DateTime.now().toUTC()} [ERROR] ${this._clean(msg)}\n`)
   }
 }
