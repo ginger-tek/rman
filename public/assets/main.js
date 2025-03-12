d1890c9b0c2772fc1fe936a80603893c4adebdc3
ginger-tek <moormann.jeremy.tech@gmail.com>
2025-03-11 23:57:18 -0400

    icons + session store impl

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,6 +1,8 @@
 import store from './store.js'
 import router from './router.js'
 
+store.user = await fetch('/api/auth/session').then(r=>r.json())
+
 Vue.createApp({
   template: `<header>
     <nav>
@@ -9,16 +11,18 @@ Vue.createApp({
         <li><router-link to="/dashboard"><i class="bi bi-bar-chart-steps"></i> Dashboard</router-link></li>
         <li><router-link to="/projects"><i class="bi bi-card-heading"></i> Projects</router-link></li>
         <li><router-link to="/resources"><i class="bi bi-people"></i> Resources</router-link></li>
+        <li><router-link to="/approvals"><i class="bi bi-person-check-fill"></i> Approvals</router-link></li>
+        <li><router-link to="/my-approvals"><i class="bi bi-person-check-fill"></i> My Approvals</router-link></li>
       </ul>
       <ul>
         <li><pv-dark-mode></pv-dark-mode></li>
         <li>
           <details class="dropdown">
-            <summary><i class="bi bi-person-circle"></i> User</summary>
+            <summary><i class="bi bi-person-circle"></i> {{ store.user.name }}</summary>
             <ul dir="rtl">
-              <li><router-link to="/settings">Settings</router-link></li>
+              <li><router-link to="/settings">Settings <i class="bi bi-gear"></i></router-link></li>
               <li><hr></li>
-              <li><router-link to="/logout">Logout</router-link></li>
+              <li><router-link to="/logout">Logout <i class="bi bi-box-arrow-right"></i></router-link></li>
             </ul>
           </details>
         </li>
@@ -34,6 +38,6 @@ Vue.createApp({
     return { route, store }
   }
 })
-.use(router)
-.use(PicoVue)
-.mount('#app')
+  .use(router)
+  .use(PicoVue)
+  .mount('#app')
